5f4eed8712a387c504aa3fca4d6a6eb570324538
Morozova Kate <morozova1861@mail.ru>
2023-11-13 17:09:38 +0600

    the template base has been taken out

--- city_newsproject/city_newsproject/settings.py
+++ city_newsproject/city_newsproject/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'city_newsproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,7 +125,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
