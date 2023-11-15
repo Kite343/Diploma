8cdee55f449a5faa0bfb532b4f037aa4a8d0aaa6
Morozova Kate <morozova1861@mail.ru>
2023-11-15 19:24:31 +0600

    profile user

--- city_newsproject/users/urls.py
+++ city_newsproject/users/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     # path('logout/', views.logout_user, name='logout'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterUser.as_view(), name='register'),
+    path('profile/', views.ProfileUser.as_view(), name='profile'),
 ]
