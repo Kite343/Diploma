0372d63f5e8c4b2f15fc8af8350596daddb52c7b
Morozova Kate <morozova1861@mail.ru>
2023-11-16 15:44:17 +0600

    captcha

--- city_newsproject/users/forms.py
+++ city_newsproject/users/forms.py
@@ -5,6 +5,7 @@ from django.utils.deconstruct import deconstructible
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+from captcha.fields import CaptchaField
 
 class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
@@ -19,6 +20,7 @@ class RegisterUserForm(UserCreationForm):
     username = forms.CharField(label="Логин", widget=forms.TextInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label="Повтор пароля", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    captcha = CaptchaField(label='Введите текст с картинки')
 
     class Meta:
         model = get_user_model()
